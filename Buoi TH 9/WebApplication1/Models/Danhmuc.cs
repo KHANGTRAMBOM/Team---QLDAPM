1ec277c16387941ff9b55d32a5e618ffeba27e3a
NguyenDuongTrucVy <vy_dth216254@student.agu.edu.vn>
2024-12-13 15:47:24 +0700

    cập nhật đồ án

--- Buoi TH 9/WebApplication1/Models/Danhmuc.cs
+++ Buoi TH 9/WebApplication1/Models/Danhmuc.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
@@ -9,12 +10,19 @@ namespace WebApplication1.Models;
 [Table("DANHMUC")]
 public partial class Danhmuc
 {
+    [DisplayName("Mã danh mục")]
     [Key]
     [Column("MaDM")]
     public int MaDm { get; set; }
+    
+    [DisplayName("Tên danh mục")]
+    [Required(ErrorMessage = "Tên danh mục không được bỏ trống.")]
+    [StringLength(255)]
+    public required string Ten { get; set; }
 
-    [StringLength(100)]
-    public string Ten { get; set; } = null!;
+    [DisplayName("Tên danh mục không dấu")]
+    [StringLength(255)]
+    public string? TenKhongDau { get; set; }
 
     [InverseProperty("MaDmNavigation")]
     public virtual ICollection<Mathang> Mathangs { get; set; } = new List<Mathang>();
