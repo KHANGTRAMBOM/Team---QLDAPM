680e40fcacc4ca39115250ebeca197a2fae82a89
Bunbo <mynametanker@gmail.com>
2024-12-14 22:52:14 +0700

    Thêm seed data cho bảng danh mục, mặt hàng và cửa hàng

--- Buoi TH 9/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Buoi TH 9/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ab8daa9260257f4df6f30e627850a0e0b7b4d89")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
