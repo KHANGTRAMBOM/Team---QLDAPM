0feb311a25694d65150fe2ae55d72f3e4a816c2e
Thư <thu_dth216288@student.agu.edu.vn>
2024-12-18 23:24:33 +0700

    cập nhật views/mathangs/edit.cshtml

--- Buoi TH 9/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Buoi TH 9/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f75b6ae7265f7c50d573fed09e184fded0d3082c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41857c57ec5ea3d930efd7d15302e0587ba60a0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
