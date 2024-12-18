d374cbeddcbaf401101a3e2ba51e8172daf468e0
Bunbo <mynametanker@gmail.com>
2024-12-19 00:16:51 +0700

    Bản final của nhóm

--- Buoi TH 9/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Buoi TH 9/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41857c57ec5ea3d930efd7d15302e0587ba60a0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af5e09f8643753a450e1ec4ea8f0c07005503ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
